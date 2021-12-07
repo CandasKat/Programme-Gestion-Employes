f4fa0b07bcca043bee92a66ab2a777692f34fa2a
CandasKat <hrisohest@gmail.com>
2021-12-07 19:14:20 +0100

    qw

---
+++ src/ActionEmployes.java
@@ -0,0 +1,69 @@
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author candas
+ */
+public class ActionEmployes {
+    private static Connection con = null;
+    
+    private static Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+
+
+
+    static ResultSet yap(){
+        try {
+            statement = con.createStatement();
+            String sorgu = "Select * From calisanlar";
+            ResultSet rs;
+            rs = statement.executeQuery(sorgu);
+            return rs;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+
+    public boolean connexion(String utilisateur, String password){
+        String sorgu = "Select * From adminler where username = ? and password = ?";
+        try {
+            preparedStatement = con.prepareStatement(sorgu);
+            preparedStatement.setString(1, utilisateur);
+            preparedStatement.setString(2, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+
+            return resultSet.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public ActionEmployes(){
+        String url = "jdbc:mysql://" + Database.host + ":" + Database.port + "/" + Database.nom_db;
+
+        try {            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Pilote introuvable...");
+        }
+        try{
+            con = DriverManager.getConnection(url, Database.utilisateur, Database.password);
+            System.out.println("Connexion réussie...");
+        }catch (Exception ex) {
+             System.out.println("La connexion a échoué");
+        }
+    }
+
+
+}
---
+++ src/Database.java
@@ -0,0 +1,17 @@
+
+/**
+ *
+ * @author candas
+ */
+public class Database {
+    public static final String utilisateur = "root";
+    public static final String password = "";
+    
+    public static final  String nom_db = "demo";
+    
+    public static final String host = "localhost";
+    
+    public static final int port = 3306;
+    
+    
+}
