de61423b989a5fd5f65a38b76af3252beee781df
CandasKat <88893818+CandasKat@users.noreply.github.com>
2021-12-09 08:33:01 +0100

    Update EcranEmployes.java

--- src/EcranEmployes.java
+++ src/EcranEmployes.java
@@ -1,116 +1,253 @@
 import javax.swing.*;
+import javax.swing.event.CellEditorListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.EventObject;
 
-public class EcranConnexion extends JFrame{
-    JFrame frame = new JFrame("Connexion");
-    JPanel panel1 = new JPanel();
-    JLabel label1 = new JLabel("Utilisateur : ");
-    JLabel label2 = new JLabel("Mot de Passe : ");
-    JTextField utilisateurChamp = new JTextField();
-    JPasswordField passwordChamp = new JPasswordField();
-    JLabel messageChamp = new JLabel();
-    JCheckBox checkBox = new JCheckBox("Afficher mot de passe");
-    JButton connexionButton = new JButton("Connection");
+public class EcranEmployes extends JDialog implements CellEditor{
+    private JTable tableEmployes = new JTable();
 
-    public EcranConnexion() {
-        frame.setSize(500, 300);
-        frame.setLayout(null);
-        panel1.setLayout(null);
-        frame.setContentPane(panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        frame.setLocationRelativeTo(null);
 
-        //label Utilisateur
-        panel1.add(label1);
-        label1.setBounds(50, 45, 100, 25);
-        label1.setSize(100,25);
 
-        //texField Utilisateur
-        panel1.add(utilisateurChamp);
-        utilisateurChamp.setBounds(150, 45, 295, 25);
-        utilisateurChamp.setSize(295,25);
 
-        //label mot de passe
-        panel1.add(label2);
-        label2.setBounds(50, 80, 100, 25);
-        label2.setSize(100,25);
+    public void dynamiqueChercher(String cherche){
+        //chercher sur le tableau
 
-        //password field
-        panel1.add(passwordChamp);
-        passwordChamp.setBounds(150, 80, 295, 25);
-        passwordChamp.setSize(295,25);
+        TableRowSorter<DefaultTableModel> tr =new TableRowSorter<DefaultTableModel>(modelim);
+        tableEmployes.setRowSorter(tr);
+        tr.setRowFilter(RowFilter.regexFilter(cherche));
+    }
 
-        //checkbox
-        panel1.add(checkBox);
-        checkBox.setBounds(50, 115, 180, 25);
-        checkBox.setSize(180,25);
+    DefaultTableModel modelim = new DefaultTableModel();
 
-        //label mot de passe
-        panel1.add(messageChamp);
-        messageChamp.setBounds(50, 150, 400, 25);
-        messageChamp.setSize(400,25);
-        messageChamp.setForeground(Color.RED);
-        messageChamp.setText("");
+    @Override
+    public Object getCellEditorValue() {
+        return null;
+    }
 
-        //connection button
-        panel1.add(connexionButton);
-        connexionButton.setBounds(125, 185, 250, 25);
-        connexionButton.setSize(250,25);
+    @Override
+    public boolean isCellEditable(EventObject anEvent) {
+        return false;
+    }
 
-        //checkbox utilisation
-        checkBox.addActionListener(new ActionListener() {
+    @Override
+    public boolean shouldSelectCell(EventObject anEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean stopCellEditing() {
+        return false;
+    }
+
+    @Override
+    public void cancelCellEditing() {
+
+    }
+
+    @Override
+    public void addCellEditorListener(CellEditorListener l) {
+
+    }
+
+    @Override
+    public void removeCellEditorListener(CellEditorListener l) {
+
+    }
+
+
+    Object[] columns = {"Id", "Nom", "Prénom", "Département", "Salaire"};
+    Object[] rows = new Object[5];
+
+    ActionEmployes actions = new ActionEmployes();
+
+
+    public EcranEmployes() {
+
+        JFrame jf = new JFrame("Table d'employes");
+        jf.setLayout(null);
+
+        //cherche field
+        JTextField cherheField = new JTextField("");
+        cherheField.setBounds(25,30,450,25);
+        cherheField.setSize(450,25);
+        cherheField.setLocation(25,30);
+        jf.add(cherheField);
+
+        cherheField.addKeyListener(new KeyListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                if (checkBox.isSelected()){
-                    passwordChamp.setEchoChar((char) 0);
-                }
-                else {
-                    passwordChamp.setEchoChar('*');
-                }
+            public void keyTyped(KeyEvent e) {
+
             }
-        });
-
-        connexionButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ActionEmployes actionEmployes = new ActionEmployes();
-                messageChamp.setText("");
-
-                String utilisateur = utilisateurChamp.getText();
-                String password = new String(passwordChamp.getPassword());
-
-
-                boolean connexionReussi = actionEmployes.connexion(utilisateur, password);
-
-                if (connexionReussi){
-                    setVisible(false);
-                    EcranEmployes ecranEmployes = new EcranEmployes();
-
-                    ecranEmployes.setVisible(true);
-                    //System.exit(0);
-                }
-                else messageChamp.setText("Échec de la connexion... Veuillez réessayer");
-            }
-        });
-        connexionButton.addKeyListener(new KeyAdapter() {
 
             @Override
             public void keyPressed(KeyEvent e) {
-                EcranConnexion ecranConnexion = new EcranConnexion();
-                int touch = e.getKeyCode();
-                if (touch == KeyEvent.VK_ENTER){
-                    ecranConnexion.connexionButton.doClick();
-                }
-
 
             }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                String cherche = cherheField.getText();
+                dynamiqueChercher(cherche);
+            }
         });
+
+
+
+        //Separateur
+        JSeparator separator = new JSeparator();
+        separator.setBounds(25,80, 640,2);
+        separator.setForeground(Color.GRAY);
+        jf.add(separator);
+
+        //label nom
+        JLabel labelNom = new JLabel("Nom : ");
+        labelNom.setBounds(25,110,70,25);
+        labelNom.setSize(70,25);
+        labelNom.setLocation(25,110);
+        jf.add(labelNom);
+
+        //textField de nom
+        JTextField nomField = new JTextField();
+        nomField.setBounds(130, 110, 345,25);
+        nomField.setSize(345,25);
+        nomField.setLocation(130,110);
+        jf.add(nomField);
+
+        //Ajouter button
+        JButton ajouterButton = new JButton("Ajouter");
+        ajouterButton.setBounds(530,110,100,25);
+        ajouterButton.setSize(100,25);
+        ajouterButton.setLocation(530, 110);
+        jf.add(ajouterButton);
+
+
+
+        //label prénom
+        JLabel labelPrenom = new JLabel("Prénom : ");
+        labelPrenom.setBounds(25,150,70,25);
+        labelPrenom.setSize(70,25);
+        labelPrenom.setLocation(25,150);
+        jf.add(labelPrenom);
+
+        //textField de prénom
+        JTextField prenomField = new JTextField();
+        prenomField.setBounds(130, 150, 345,25);
+        prenomField.setSize(345,25);
+        prenomField.setLocation(130,150);
+        jf.add(prenomField);
+
+        //label département
+        JLabel labelDepartement = new JLabel("Département : ");
+        labelDepartement.setBounds(25,190,90,25);
+        labelDepartement.setSize(90,25);
+        labelDepartement.setLocation(25,190);
+        jf.add(labelDepartement);
+
+        //textField de département
+        JTextField departementField = new JTextField();
+        departementField.setBounds(130, 190, 345,25);
+        departementField.setSize(345,25);
+        departementField.setLocation(130,190);
+        jf.add(departementField);
+
+        //label salaire
+        JLabel labelSalaire = new JLabel("Salaire : ");
+        labelSalaire.setBounds(25,230,90,25);
+        labelSalaire.setSize(90,25);
+        labelSalaire.setLocation(25,230);
+        jf.add(labelSalaire);
+
+        //textField de salaire
+        JTextField salaireField = new JTextField();
+        salaireField.setBounds(130, 230, 345,25);
+        salaireField.setSize(345,25);
+        salaireField.setLocation(130,230);
+        jf.add(salaireField);
+
+        //label message
+        JLabel messageLabel = new JLabel("");
+        messageLabel.setBounds(25,270,620,25);
+        messageLabel.setSize(620,25);;
+        messageLabel.setLocation(25,270);
+        messageLabel.setForeground(Color.RED);
+        jf.add(messageLabel);
+
+
+
+
+        //tableau
+        modelim.setColumnIdentifiers(columns);
+
+
+        JScrollPane sp = new JScrollPane();
+        sp.setSize(650,500);
+        sp.setLocation(25,300);
+        sp.setViewportView(tableEmployes);
+
+        jf.add(sp);
+        ajouteTable();
+
+        jf.setSize(700,850);
+        jf.setLocationRelativeTo(null);
+        jf.setVisible(true);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+        //ajouter button action
+        ajouterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nom = nomField.getText();
+                String prenom = prenomField.getText();
+                String departement = departementField.getText();
+                String salaire = salaireField.getText();
+
+                actions.ajoutePersonne(nom, prenom, departement, salaire);
+
+                ajouteTable();
+
+                messageLabel.setText("Nouvel employé ajouté avec succès");
+            }
+        });
+
+    }
+    //ajouter à table
+    public void ajouteTable(){
+        ResultSet rs = actions.yap();
+
+
+        try {
+            while (rs.next()){
+                rows[0] = rs.getString("id");
+                rows[1] = rs.getString("soyad");
+                rows[2] = rs.getString("ad");
+                rows[3] = rs.getString("departman");
+                rows[4] = rs.getString("maas");
+                modelim.addRow(rows);
+
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+
+
+        tableEmployes.setEnabled(false);
+        tableEmployes.setModel(modelim);
+    }
+
+    public static void main(String[] args) {
+        new EcranEmployes();
     }
 
 
 }
+
+
