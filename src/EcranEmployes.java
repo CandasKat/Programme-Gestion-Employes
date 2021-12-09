b89244f5e609ab3023c0b906a42218bc4db68b54
CandasKat <88893818+CandasKat@users.noreply.github.com>
2021-12-09 15:36:43 +0100

    Update EcranEmployes.java

--- src/EcranEmployes.java
+++ src/EcranEmployes.java
@@ -1,14 +1,13 @@
 import javax.swing.*;
-import javax.swing.event.CellEditorListener;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
 import java.awt.event.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.EventObject;
 
-public class EcranEmployes extends JDialog implements CellEditor{
+public class EcranEmployes extends JFrame {
     private JTable tableEmployes = new JTable();
 
 
@@ -22,42 +21,12 @@ public class EcranEmployes extends JDialog implements CellEditor{
         tr.setRowFilter(RowFilter.regexFilter(cherche));
     }
 
-    DefaultTableModel modelim = new DefaultTableModel();
+    DefaultTableModel modelim = new DefaultTableModel() {
+      public boolean isCellEditable(int row, int column) {
+            return false;
+        }
+    };
 
-    @Override
-    public Object getCellEditorValue() {
-        return null;
-    }
-
-    @Override
-    public boolean isCellEditable(EventObject anEvent) {
-        return false;
-    }
-
-    @Override
-    public boolean shouldSelectCell(EventObject anEvent) {
-        return false;
-    }
-
-    @Override
-    public boolean stopCellEditing() {
-        return false;
-    }
-
-    @Override
-    public void cancelCellEditing() {
-
-    }
-
-    @Override
-    public void addCellEditorListener(CellEditorListener l) {
-
-    }
-
-    @Override
-    public void removeCellEditorListener(CellEditorListener l) {
-
-    }
 
 
     Object[] columns = {"Id", "Nom", "Prénom", "Département", "Salaire"};
@@ -67,8 +36,9 @@ public class EcranEmployes extends JDialog implements CellEditor{
 
 
     public EcranEmployes() {
-
-        JFrame jf = new JFrame("Table d'employes");
+        JFrame frame = new JFrame("Table d'employes");
+        JPanel jf = new JPanel();
+        frame.add(jf);
         jf.setLayout(null);
 
         //cherche field
@@ -141,6 +111,13 @@ public class EcranEmployes extends JDialog implements CellEditor{
         prenomField.setLocation(130,150);
         jf.add(prenomField);
 
+        //Modifier button
+        JButton modifierButton = new JButton("Modifier");
+        modifierButton.setBounds(530,150,100,25);
+        modifierButton.setSize(100,25);
+        modifierButton.setLocation(530, 150);
+        jf.add(modifierButton);
+
         //label département
         JLabel labelDepartement = new JLabel("Département : ");
         labelDepartement.setBounds(25,190,90,25);
@@ -154,6 +131,13 @@ public class EcranEmployes extends JDialog implements CellEditor{
         departementField.setSize(345,25);
         departementField.setLocation(130,190);
         jf.add(departementField);
+
+        //Supprimer button
+        JButton supprimerButton = new JButton("Supprimer");
+        supprimerButton.setBounds(530,190,100,25);
+        supprimerButton.setSize(100,25);
+        supprimerButton.setLocation(530, 190);
+        jf.add(supprimerButton);
 
         //label salaire
         JLabel labelSalaire = new JLabel("Salaire : ");
@@ -192,11 +176,10 @@ public class EcranEmployes extends JDialog implements CellEditor{
         jf.add(sp);
         ajouteTable();
 
-        jf.setSize(700,850);
-        jf.setLocationRelativeTo(null);
-        jf.setVisible(true);
-        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        frame.setSize(700,850);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
         //ajouter button action
@@ -208,11 +191,125 @@ public class EcranEmployes extends JDialog implements CellEditor{
                 String departement = departementField.getText();
                 String salaire = salaireField.getText();
 
-                actions.ajoutePersonne(nom, prenom, departement, salaire);
+                if (nom.equals("") | prenom.equals("") | departement.equals("") | salaire.equals("")){
+                    messageLabel.setText("Veuillez saisir les informations complètes");
+                }
+                else {
 
-                ajouteTable();
+                    actions.ajoutePersonne(nom, prenom, departement, salaire);
 
-                messageLabel.setText("Nouvel employé ajouté avec succès");
+                    modelim.getDataVector().removeAllElements();
+                    modelim.fireTableDataChanged();
+                    ajouteTable();
+
+                    messageLabel.setText("Nouvel employé ajouté avec succès");
+
+                    nomField.setText("");
+                    prenomField.setText("");
+                    departementField.setText("");
+                    salaireField.setText("");
+                }
+            }
+        });
+
+        //quand on click sur la table
+        tableEmployes.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int selectedrow = tableEmployes.getSelectedRow();
+                nomField.setText(tableEmployes.getValueAt(selectedrow,1).toString());
+                prenomField.setText(tableEmployes.getValueAt(selectedrow,2).toString());
+                departementField.setText(tableEmployes.getValueAt(selectedrow,3).toString());
+                salaireField.setText(tableEmployes.getValueAt(selectedrow,4).toString());
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        //modifier button action
+        modifierButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nom = nomField.getText();
+                String prenom = prenomField.getText();
+                String departement = departementField.getText();
+                String salaire = salaireField.getText();
+                int selectedRow = tableEmployes.getSelectedRow();
+
+
+
+                if (selectedRow == -1){
+                    if (modelim.getRowCount() == 0){
+                        messageLabel.setText("Le tableau est actuellement vide");
+                    }
+                    else {
+                        messageLabel.setText("Veuillez sélectionner l'employé que vous souhaitez mettre à jour");
+                    }
+                }
+                else{
+                    String string = modelim.getValueAt(selectedRow,0).toString();
+                    int id = Integer.parseInt(string);
+                    actions.metJourEmployes(id,nom,prenom,departement,salaire);
+
+                    modelim.getDataVector().removeAllElements();
+                    modelim.fireTableDataChanged();
+                    ajouteTable();
+
+                    tableEmployes.repaint();
+                    messageLabel.setText("L'employé a mis a jour avec succès");
+                    nomField.setText("");
+                    prenomField.setText("");
+                    departementField.setText("");
+                    salaireField.setText("");
+                }
+            }
+        });
+
+        //supprimer button
+        supprimerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                messageLabel.setText("");
+                int selectedrow = tableEmployes.getSelectedRow();
+                if (selectedrow == -1){
+                    if (modelim.getRowCount() == 0){
+                        messageLabel.setText("Le tableau est déjà vide");
+                    }
+                    else {
+                        messageLabel.setText("Veuillez sélectionner l'employé que vous souhaitez supprimer");
+                    }
+                }
+                else{
+                    String string = modelim.getValueAt(selectedrow,0).toString();
+                    int id = Integer.parseInt(string);
+                    actions.supprimer(id);
+
+                    modelim.getDataVector().removeAllElements();
+                    modelim.fireTableDataChanged();
+                    ajouteTable();
+                    messageLabel.setText("L'employé a été supprimé avec succès");
+                    nomField.setText("");
+                    prenomField.setText("");
+                    departementField.setText("");
+                    salaireField.setText("");
+                }
             }
         });
 
@@ -220,6 +317,7 @@ public class EcranEmployes extends JDialog implements CellEditor{
     //ajouter à table
     public void ajouteTable(){
         ResultSet rs = actions.yap();
+
 
 
         try {
@@ -238,13 +336,8 @@ public class EcranEmployes extends JDialog implements CellEditor{
         }
 
 
-
-        tableEmployes.setEnabled(false);
         tableEmployes.setModel(modelim);
-    }
 
-    public static void main(String[] args) {
-        new EcranEmployes();
     }
 
 
